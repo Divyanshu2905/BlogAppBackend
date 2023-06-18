da9acb4f531c9339c14eb9733ef6b393fdc19db1
Divyanshu Watts <rhtwatts@gmail.com>
2023-06-18 11:44:46 +0530

    For bugs

--- routes/upload.js
+++ routes/upload.js
@@ -14,6 +14,7 @@ router.put("/", async(req,res)=>{
     try{
         const savedPhoto=cloudinary.uploader.upload(newPhoto.file);
         res.status(200).json(savedPhoto);
+        console.log(savedPhoto);
     }catch(err){console.log(err)}
 })
 
